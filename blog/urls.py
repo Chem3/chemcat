2e5e230f7f6d5760ecd7a40dd272cee359c7c1d3
Chem3 <fmk@tuta.io>
2020-09-24 11:09:22 +0000

    works on search functionality

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.bottle_list, name='bottle_list'),
+    path('', views.bottle_list, name='bottle_list'),
+    path('bottle/<int:pk>/', views.bottle_detail, name='bottle_detail'),
+    path('search/', views.bottle_search, name='bottle_search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Bottles
 from django.utils import timezone
 
@@ -6,3 +6,11 @@ from django.utils import timezone
 def bottle_list(request):
     bottles = Bottles.objects.filter(registered_date__lte=timezone.now()).order_by('-registered_date')
     return render(request, 'blog/bottle_list.html', {'bottles' : bottles})
+
+def bottle_detail(request, pk):
+    bottle = get_object_or_404(Bottles, pk=pk)
+    return render(request, 'blog/bottle_detail.html', {'bottle': bottle})
+
+def bottle_search(request):
+    form = SearchForm()
+    return render(request, 'blog/chemical_search.html', {'form': form})
